8cbb5b6b0b8ac4d9c2dba54ef7e6370e37f1d799
Jorge Ramos <142628950+jorgeramos-dev@users.noreply.github.com>
2023-12-25 19:25:43 -0300

    instalacao do twig e estrutura de viewa

---
+++ app/core/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\core;
+
+class Controller
+{
+    protected function load(string $view, $params = [])
+    {
+
+    }
+
+    public function teste()
+    {
+        echo 'teste';
+    }
+}
